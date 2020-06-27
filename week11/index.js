0386a51abc977762d434226937906148ba861d1c
dzw1995 <245234166@qq.com>
2020-06-27 14:55:17 +0800

    'fixbug'

--- week11/index.js
+++ week11/index.js
@@ -8,10 +8,10 @@ let map = localStorage.getItem('map') ? JSON.parse(localStorage.getItem('map')) 
 
 function render () {
   let html = ''
-  for (let x = 0; x < 100; x++) {
-    for (let y = 0; y < 100; y++) {
+  for (let y = 0; y < 100; y++) {
+    for (let x = 0; x < 100; x++) {
       let t = map[x + 100 * y]
-      html += `<div class="cell ${t == 1 && 'active'}" dataset-x="${x}" dataset-y="${y}" dataset-active="${t}"></div>`
+      html += `<div class="cell ${t == 1 ? 'active' : ''}" dataset-x="${x}" dataset-y="${y}" dataset-active="${t}"></div>`
     }
   }
   oContainer.innerHTML = html
@@ -61,27 +61,34 @@ async function findPath (start, end, map) {
   while (queue.length) {
     let [x, y] = queue.shift()
     if (x == end[0] && y == end[1]) {
+      while (x != start[0] || y != start[1]) {
+        oContainer.children[x + 100 * y].classList.add('pink');
+        ([x, y] = map[x + 100 * y])
+      }
+      oContainer.children[x + 100 * y].classList.add('pink');
       return true
     }
-    console.log(123)
-    await insert(x - 1, y)
-    await insert(x + 1, y)
-    await insert(x, y - 1)
-    await insert(x, y + 1)
+    await insert(x - 1, y, [x, y])
+    await insert(x + 1, y, [x, y])
+    await insert(x, y - 1, [x, y])
+    await insert(x, y + 1, [x, y])
+    await insert(x - 1, y - 1, [x, y])
+    await insert(x + 1, y - 1, [x, y])
+    await insert(x - 1, y + 1, [x, y])
+    await insert(x + 1, y + 1, [x, y])
   }
   return false
-  async function insert (x, y) {
+  async function insert (x, y, pre) {
     if (x < 0 || y < 0 || x > 100 || y > 100) {
       return
     }
     if (map[x + 100 * y] != 0) {
       return
     }
-    map[x + 100 * y] = 1
+    map[x + 100 * y] = pre
     oContainer.children[x + 100 * y].classList.add('visited')
     await sleep(5)
     queue.push([x, y])
-    console.log(queue)
   }
 }
 
